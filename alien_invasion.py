2e8b4de889b313e9dcfa239377c4a617a443d724
Tsyba69 <aleksandertsybin@gmail.com>
2022-03-10 14:42:15 +0400

    Create Setting with game

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 
+from settings import Settings
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -10,22 +11,23 @@ class AlienInvasion:
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 700))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            #отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
+
+            for event in pygame.event.get():  # Отслеживание событий клавиатуры и мыши.
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Отображение последнего прорисованного экрана
-            pygame.display.flip()
+            self.screen.fill(self.settings.bg_color)  # При каждом проходе цикла перерисовывается экран
+
+            pygame.display.flip()  # Отображение последнего прорисованного экрана
 
 
 if __name__ == '__main__':
-    #создание экземпляра и запуск игры
-    ai = AlienInvasion()
+    ai = AlienInvasion()  # создание экземпляра и запуск игры
     ai.run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Класс для хранения всех настроек игры 'Alien Invasion'."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        self.screen_width = 1200  # Ширина экрана
+        self.screen_height = 700  # Высота экрана
+        self.bg_color = (230, 230, 230)  # Фон экрана

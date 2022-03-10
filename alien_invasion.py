244a185522fa24cd440c4b7ae7ef68c14c791d8d
Tsyba69 <aleksandertsybin@gmail.com>
2022-03-10 20:55:53 +0400

    Added Ship, and refactoring maincode

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -15,17 +15,25 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
+            self._check_events()
+            self._update_screen()
 
-            for event in pygame.event.get():  # Отслеживание событий клавиатуры и мыши.
-                if event.type == pygame.QUIT:
-                    sys.exit()
+    def _check_events(self):
+        """Обрабатывает нажатие клавиш и события мыши."""
+        for event in pygame.event.get():  # Отслеживание событий клавиатуры и мыши.
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            self.screen.fill(self.settings.bg_color)  # При каждом проходе цикла перерисовывается экран
-
-            pygame.display.flip()  # Отображение последнего прорисованного экрана
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран"""
+        self.screen.fill(self.settings.bg_color)  # При каждом проходе цикла перерисовывается экран
+        self.ship.blitme()
+        pygame.display.flip()  # Отображение последнего прорисованного экрана
 
 
 if __name__ == '__main__':
---
+++ ship.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Ship():
+    """класс для управления кораблем."""
+    def __init__(self, ai_game):
+        """Инициализирует корабль и задает его начальную позицию."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/ship.bmp')  # Загружает изображение корабля
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
+
